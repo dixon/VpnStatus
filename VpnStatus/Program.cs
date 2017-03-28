20677cf37389e16b2dae3dd3f7fdc68930553f30
Jarrod Dixon <jarrod.m.dixon@gmail.com>
2017-03-28 16:02:15 -0400

    initial checkin

---
+++ VpnStatus/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace VpnStatus
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (var icon = new SystemTrayIcon())
+            using (ListenForVpnStatusChanges(icon))
+            {
+                Application.Run();
+            }
+        }
+
+        private static IDisposable ListenForVpnStatusChanges(SystemTrayIcon i)
+        {
+            var t = new System.Timers.Timer();
+
+            t.Elapsed += (sender, args) => i.UpdateStatusIcon();
+            t.Interval = IntervalToPollMs;
+            t.Enabled = true;
+
+            return t;
+        }
+
+        internal static string VpnName => ConfigurationManager.AppSettings[nameof(VpnName)];
+
+        internal static int IntervalToPollMs => int.Parse(ConfigurationManager.AppSettings[nameof(IntervalToPollMs)]);
+
+        internal static bool IsVpnConnected =>
+            NetworkInterface.GetAllNetworkInterfaces()
+                            .Any(i => i.OperationalStatus == OperationalStatus.Up 
+                                   && VpnName.Equals(i.Name, StringComparison.InvariantCultureIgnoreCase));
+    }
+}
---
+++ VpnStatus/SystemTrayIcon.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VpnStatus
+{
+    internal class SystemTrayIcon : IDisposable
+    {
+        private NotifyIcon _notifyIcon;
+        private ContextMenuStrip _rightClickMenuStrip;
+
+        public SystemTrayIcon()
+        {
+            InitNotifyIcon();
+            InitRightClickMenuStrip();
+        }
+
+        public void Dispose()
+        {
+            _rightClickMenuStrip?.Dispose();
+            _notifyIcon?.Dispose();
+        }
+
+        public void UpdateStatusIcon() => _notifyIcon.Icon = StatusIcon;
+
+        private void InitNotifyIcon()
+        {
+            _notifyIcon = new NotifyIcon
+            {
+                Text = "VPN Status for " + Program.VpnName,
+                Visible = true
+            };
+
+            // left-clicking icon in tray should open Control Panel > Network Connections
+            _notifyIcon.MouseClick += (o, args) => { if (args.Button == MouseButtons.Left) { Process.Start("ncpa.cpl"); } };
+
+            UpdateStatusIcon();
+        }
+
+        private void InitRightClickMenuStrip()
+        {
+            _rightClickMenuStrip = new ContextMenuStrip();
+
+            var item = new ToolStripMenuItem("Exit");
+            item.Click += (sender, args) => Application.Exit();
+            _rightClickMenuStrip.Items.Add(item);
+
+            _notifyIcon.ContextMenuStrip = _rightClickMenuStrip;
+        }
+
+        private static Icon StatusIcon => Program.IsVpnConnected ? Resources.icon_connected : Resources.icon_disconnected;
+    }
+}
